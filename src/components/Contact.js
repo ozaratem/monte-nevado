e3ab57a1c8cdec3d73e088a1f5e445e4a401be56
ozaratem <zaratemorera.of@gmail.com>
2022-01-23 16:38:15 -0600

    fix contact and reservation components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,14 +4,14 @@ function Contact() {
     return <section id="contact">
     <div><h3>Contacto</h3></div>
     <div>
-      <p>Nos puede escribir sin ningun compromiso por cualquier de nuestras redes sociales o por nuestro <span class="green">chat en linea</span></p>
+      <p>Nos puede escribir sin ningun compromiso por cualquier de nuestras redes sociales o por nuestro <span className="green">chat en linea</span></p>
     </div>
     <div>
     <div>
-      <button className="button"><i class="fab fa-google-plus-g"></i> GOOGLE+</button>
+      <button className="button"><i className="fab fa-google-plus-g"></i> GOOGLE+</button>
     </div>
     <div>
-      <button className="button"><i class="fab fa-twitter"></i> TWITTER</button></div>
+      <button className="button"><i className="fab fa-twitter"></i> TWITTER</button></div>
     <div>
       <button className="button">CHAT</button> </div></div>
   </section>
--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -2,53 +2,57 @@ import React from 'react';
 
 function Reservation() {
     return <section id="reservation">
-    <div className="h3"><h3>Reservaciones</h3></div>
-    <div className="reservation-input">
-      <div>
-        <p>Nombre completo</p>
-        <input className="input" type="text"/>
-      </div>
-      <div>
-        <p>Fecha de entrada</p>
-        <input className="input" type="date"/>
-      </div>
-      <div>
-        <p>Fecha de salida</p>
-        <input className="input" type="date"/>
-      </div>
-      <div>
-        <p>Email</p>
-        <input className="input" type="email"/>
-      </div>
-      <div>
-        <p>Telefono</p>
-        <input className="input" type="text"/>
-      </div>
-    </div>
-    <div className="reservation-checkbox">
-      <p>Tipo de moneda con la que va a realizar el pago</p>
-        <div>
-          <input className="round" type="checkbox" name="Colones"/>
-          <label for="Colones">Colones</label>
+      <div className="container">
+        <div className="h3">
+          <h3>Reservaciones</h3>
         </div>
-        <div>
-          <input className="round" type="checkbox" name="Dolares"/>
-          <label for="Dolares">Dolares</label>
+        <div className="reservation-input">
+          <div>
+            <p>Nombre completo</p>
+            <input className="input-group" type="text"/>
+          </div>
+          <div>
+            <p>Fecha de entrada</p>
+            <input className="input-group" type="date"/>
+          </div>
+          <div>
+            <p>Fecha de salida</p>
+            <input className="input-group" type="date"/>
+          </div>
+          <div>
+            <p>Email</p>
+            <input className="input-group" type="email"/>
+          </div>
+          <div>
+            <p>Telefono</p>
+            <input className="input-group" type="text"/>
+          </div>
         </div>
-        <div>
-          <input className="round" type="checkbox" name="Euros"/>
-          <label for="Euros">Euros</label>
+        <div className="reservation-checkbox">
+          <p>Tipo de moneda con la que va a realizar el pago</p>
+          <div>
+            <input id="colones" type="radio" name="currency" value="colones"/>
+            <label htmlFor="colones">Colones</label>
+          </div>
+          <div >
+            <input id="dolares" type="radio" name="currency" value="dolares"/>
+            <label htmlFor="dolares">Dolares</label>
+          </div>
+          <div>
+            <input id="euros" type="radio" name="currency" value="euros"/>
+            <label htmlFor="euros">Euros</label>
+          </div>
         </div>
-    </div>
-    <div className="terms-input">
-      <input type="checkbox" name="terms"/>
-      <label for="terms">Acepto los terminos y condiciones</label>
-    </div>
-    <div className="reservation-button">
-      <button className="button">
-        Reservar
-      </button>
-    </div>
+          <div className="terms-input">
+            <input type="checkbox" name="terms"/>
+            <label htmlFor="terms">Acepto los terminos y condiciones</label>
+          </div>
+          <div className="reservation-button">
+            <button className="button">
+              Reservar
+            </button>
+          </div>
+      </div>
   </section>
 }
 
